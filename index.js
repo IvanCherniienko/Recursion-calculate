e440db15ea405b8aa12612fc6ec30d61734a333e
IvanCherniienko <111380678+IvanCherniienko@users.noreply.github.com>
2023-02-23 18:29:19 +0200

    ready release

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ function pow(num, degree) {
         return 1
     } else if (degree === 1) {
         return num
-    } else if (degree < 0) {
-        return pow(1 / num, -degree)
     } else {
         return num * pow(num, degree - 1)
     }
